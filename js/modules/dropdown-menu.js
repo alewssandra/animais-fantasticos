f34ff9ff5e96ae36c68ef2a819ace63888378eca
Alessandra Lima <alessandra.lo@aluno.ifsc.edu.br>
2022-04-09 19:46:20 -0300

    clickoutside

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,3 +1,5 @@
+import outsideClick from './outsideclick.js';
+
 export default function initDropdownMenu() {
   const dropdownMenus = document.querySelectorAll('[data-dropdown]');
   dropdownMenus.forEach(menu => {
@@ -9,5 +11,8 @@ export default function initDropdownMenu() {
   function handleClick(event) {
     event.preventDefault();
     this.classList.add('active');
+    outsideClick(this, ['touchstart', 'click'], () => {
+      this.classList.remove('active');
+    });
   };
 }
