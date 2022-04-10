08d47d8b30d29ccc99ab0a50e2ab39b962d69944
Alessandra Lima <alessandra.lo@aluno.ifsc.edu.br>
2022-04-09 21:00:02 -0300

    menumobile

--- js/modules/tooltip.js
+++ js/modules/tooltip.js
@@ -4,18 +4,18 @@ export default function initTooltip() {
   tooltips.forEach((item) => {
     item.addEventListener('mouseover', onMouseOver);
   })
-
+  
   function onMouseOver(event) {
     const tooltipBox = criarTooltipBox(this);
-
+    
     onMouseMove.tooltipBox = tooltipBox;
     this.addEventListener('mousemove', onMouseMove);
-
+  
     onMouseLeave.tooltipBox = tooltipBox;
     onMouseLeave.element = this;
     this.addEventListener('mouseleave', onMouseLeave);
   }
-
+  
   const onMouseLeave = {
     handleEvent() {
       this.tooltipBox.remove();
@@ -23,14 +23,14 @@ export default function initTooltip() {
       this.element.removeEventListener('mousemove', onMouseMove);
     }
   }
-
+  
   const onMouseMove = {
     handleEvent(event) {
       this.tooltipBox.style.top = event.pageY + 20 + 'px';
       this.tooltipBox.style.left = event.pageX + 20 + 'px';
     }
   }
-
+  
   function criarTooltipBox(element) {
     const tooltipBox = document.createElement('div');
     const text = element.getAttribute('aria-label');
